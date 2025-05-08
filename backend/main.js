6cccd2b980547690b1accd044aa940effb9a2edb
zaydbkhan <zaydbkhan@gmail.com>
2025-05-07 20:06:11 -0700

    working create endpoints

--- backend/main.js
+++ backend/main.js
@@ -4,16 +4,73 @@ import { PrismaClient } from './generated/prisma/index.js'
 const app = express();
 const prisma = new PrismaClient();
 
+// application/json parser
+app.use(express.json());
+
+
 app.get("/", async (req, res) => {
-  await prisma.hello.create({
-    data: {
-      magic_word: "Hello World"
-    }
-  });
-  res.send("Hello World");
+  res.send("There is nothing here.");
 });
 
-app.use(express.static("../dist"))
+app.post("/createUser", async (req, res) => {
+  if (!req.body) {
+    // Bad Request
+    res.sendStatus(400)
+    return;
+  }
+
+  try {
+    let now = new Date()
+    await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+        create_dt: now,
+        update_dt: now,
+      }
+    })
+    // OK
+    res.sendStatus(201)
+  } catch (e) {
+    // Bad Request
+    console.log(e)
+    res.sendStatus(400)
+  }
+
+})
+
+app.post("/createEvent", async (req, res) => {
+  if (!req.body) {
+    // Bad Request
+    res.sendStatus(400)
+    return;
+  }
+
+  try {
+    let now = new Date()
+    await prisma.event.create({
+      data: {
+        name: req.body.name,
+        description: req.body.description,
+        start_dt: new Date(req.body.start_dt),
+        end_dt: new Date(req.body.end_dt),
+        create_dt: now,
+        update_dt: now,
+        // this automatically creates the foreign key relation
+        userId: req.body.userId
+      }
+    })
+    // OK
+    res.sendStatus(201)
+  } catch (e) {
+    // Bad Request
+    console.log(e)
+    res.sendStatus(400)
+  }
+
+})
+
+app.use(express.static("../dist"));
 
 try {
   console.log("Now listening on port 3000");
