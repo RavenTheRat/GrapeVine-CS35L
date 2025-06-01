123a2dab426d1b6d3ab0c115d4fa5e45c8cd9cf4
zaydbkhan <zaydbkhan@gmail.com>
2025-05-31 22:12:31 -0700

    connect friend add form to backend

--- frontend/src/AddFriend.jsx
+++ frontend/src/AddFriend.jsx
@@ -9,16 +9,24 @@ function AddFriend() {
   // State containing array of users that are being searched for
   const [displayedUsers, setDisplayedUsers] = useState([]);
 
-  // Searching algorithm
-  // Will need to be updated to allow for searching users in database
-  const searchForFriends = (e) => {
-    e.preventDefault();
-
-    const filteredItems = otherUsers.filter((user) =>
-      user.name.toLowerCase().includes(searchAddFriend.toLowerCase()),
-    );
-
-    setDisplayedUsers(filteredItems);
+  const handleSubmit = async (e, onSuccess) => {
+    e.preventDefault(); // Prevent form submission from reloading the page
+    if (searchAddFriend) {
+      axios
+        .post("http://localhost:3000/friends/add", {
+          email: searchAddFriend.toLowerCase()
+        })
+        .then((response) => {
+          setSearchAddFriend("");
+          onSuccess();
+        })
+        .catch((error) => {
+          console.log(error);
+          onSuccess();
+        });
+    } else {
+      alert("Please fill in all fields.");
+    }
   };
 
   return (
@@ -29,7 +37,7 @@ function AddFriend() {
             <div className="modal">
               <div className="side-bar">
                 <h2>Add Friends</h2>
-                <form onSubmit={searchForFriends}>
+                <form onSubmit={(e) => handleSubmit(e, close)}>
                   <input
                     type="text"
                     placeholder="Enter an email address"
@@ -37,7 +45,7 @@ function AddFriend() {
                     onChange={(e) => setSearchAddFriend(e.target.value)}
                   />
                   <button type="submit" style={{ marginLeft: "5px" }}>
-                    Search
+                    Add
                   </button>
                 </form>
                 <ul style={{ listStyleType: "none" }}>
