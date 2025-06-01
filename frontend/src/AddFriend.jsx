6ac0a4ef7f0bf8cca67c043561e9d3b8b644455e
ade-lisa <adelisanavarro.1@gmail.com>
2025-06-01 16:44:17 -0700

    Adding friends now works on the frontend. Some minor changes to be done later!

--- frontend/src/AddFriend.jsx
+++ frontend/src/AddFriend.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import "reactjs-popup/dist/index.css";
 import Popup from "reactjs-popup";
 import './styles.css'
+import axios from "axios";
 
+/*
 function AddFriend() {
   // State containing text when searching for a friend to add
   const [searchAddFriend, setSearchAddFriend] = useState("");
@@ -51,6 +53,109 @@ function AddFriend() {
               </div>
             </div>
           </>
+        )}
+      </Popup>
+    </div>
+  );
+}
+  */
+
+
+/*
+
+function AddFriend() {
+  const [email, setEmail] = useState("");
+
+  const handleAddFriend = async (e, onSuccess) => {
+    e.preventDefault(); // Prevent form submission from reloading the page
+     
+    axios
+    .post("http://localhost:3000/friends/add", {
+      email }
+    )
+  }
+
+  return (
+    <div className = "add-friend">
+      <Popup trigger = {<button> Add Friend!</button>} modal nested>
+        {(close) => (
+          <div className = "modal">
+            <h1>Add Friend</h1>
+            <form onSubmit = {handleAddFriend}>
+              <input
+                type = "email"
+                placeholder = "Enter friend's email!"
+                value = {email}
+                onChange = {(e) => setEmail(e.target.value)}
+                required
+              />
+              <button type = "submit"> 
+                Send Friend Request!
+              </button>
+            </form>
+          </div>
+        )}
+      </Popup>
+    </div>
+
+  );
+}
+
+export default AddFriend;
+
+*/
+
+function AddFriend() {
+  const [email, setEmail] = useState("");
+  const [friendStatus, setFriendStatus] = useState(""); // used for message display: let user know if request was sent
+
+  const handleAddFriend = async (e) => {
+    e.preventDefault();       // to avoid reloading the page ig
+    if (!email) {
+      setFriendStatus("Please enter an email.");
+      return;
+    }
+    axios
+    .post("http://localhost:3000/friends/add", {
+      email
+    })
+
+    .then((response) => {
+      if (response.status === 200) {
+        setFriendStatus("Friend request sent!");
+      }
+    })
+
+    .catch((error) => {
+      setFriendStatus("There was an error! Please try again!");
+    })
+  }
+
+  return (
+    <div className="side-bar-add">
+      <Popup trigger={<button>Add Friend!</button>} modal nested>
+        {(close) => (
+          <div className="modal">
+            <div className="side-bar">
+              <h2>Add Friend</h2>
+              <form onSubmit={handleAddFriend}>
+                <input
+                  type="email"
+                  placeholder="Enter an email address!"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <button type="submit">
+                  Send Request!
+                </button>
+              </form>
+              {friendStatus && <p>{friendStatus}</p>}
+            </div>
+            <div>
+              <button onClick={() => close()}>Close</button>
+            </div>
+          </div>
         )}
       </Popup>
     </div>
