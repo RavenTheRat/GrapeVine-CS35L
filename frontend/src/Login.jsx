e40c5cff6700490895d8448867629d5ca26c664f
ade-lisa <adelisanavarro.1@gmail.com>
2025-06-01 17:44:25 -0700

    Switched over to formatting/styling and editing small details instead b/c the friend add form had already been done on another branch. Whoops!

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,15 +1,22 @@
 
 // make this prettier later!
 function Login() {
-    return (
-    <button
-            onClick={() => {
-              window.location.href = "http://localhost:3000/login";
-            }}
-          >
-            Login
-          </button>
-    );
+  return (
+    <div className = "login-page">
+      <div className = "welcome-message">
+        Welcome to Grapevine!
+      </div>
+      <div>
+      <button
+        onClick={() => {
+          window.location.href = "http://localhost:3000/login";
+        }} className = "login-button"
+      >
+        Login
+      </button>
+      </div>
+      </div>
+  );
 }
 
 export default Login;
